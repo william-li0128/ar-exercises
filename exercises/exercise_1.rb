3bb4871c219075fbebd72107f515323bcaab8178
William Li <110069721+william-li0128@users.noreply.github.com>
2023-03-09 12:11:56 -0800

    finish exercise_2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -10,9 +10,9 @@ class Store < ActiveRecord::Base
 end
 
 # create three store
-burnaby =  Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+store =  Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 # output the number of stores
 puts Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the first store
+@store1 = Store.find(1)
+
+# Load the second store
+@store2 = Store.find_by(id: 2)
+
+# Update @store1 instance in the database
+@store1.update(name: "BurnabyBranch")
