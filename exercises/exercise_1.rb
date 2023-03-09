e7f054e23fbb601a716e801a5941b0044bbe7fd3
William Li <110069721+william-li0128@users.noreply.github.com>
2023-03-09 11:57:38 -0800

    finish exercise_1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# create Active Record models
+class Store < ActiveRecord::Base
+end
+
+# create three store
+burnaby =  Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# output the number of stores
+puts Store.count
